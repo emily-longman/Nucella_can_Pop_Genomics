0e59cf8111e9a7a691b1fae36bc5db6d920bc4a7
Emily Longman <Emily.Longman@uvm.edu>
2024-12-30 09:22:15 -0500

    Update 00_download_reads.sh

--- src/01_fastq_to_vcf/00_download_reads.sh
+++ src/01_fastq_to_vcf/00_download_reads.sh
@@ -43,6 +43,12 @@ rsync -avL slimsdata.genomecenter.ucdavis.edu::slims/xtzg2b6e8n/ .
 
 # Download short reads to external hard drive
 
+# Move to the directory where you want the output files to be saved
 #cd /Volumes/One\ Touch/Nucella_population_genomics
 
+# Use rsync to download the data
+## First lane of data:
 #rsync -avL slimsdata.genomecenter.ucdavis.edu::slims/w97dompzyc/ .
+
+## Second lane of data:
+#rsync -avL slimsdata.genomecenter.ucdavis.edu::slims/xtzg2b6e8n/ .
