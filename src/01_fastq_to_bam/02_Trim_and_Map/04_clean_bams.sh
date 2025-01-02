83d1841a08b7fad269e02156cbc961f35bb85546
Emily Longman <Emily.Longman@uvm.edu>
2025-01-02 07:40:44 -0500

    Update 04_clean_bams.sh

--- src/01_fastq_to_bam/02_Trim_and_Map/04_clean_bams.sh
+++ src/01_fastq_to_bam/02_Trim_and_Map/04_clean_bams.sh
@@ -15,10 +15,10 @@
 #SBATCH --ntasks-per-node=1
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=1:00:00 
+#SBATCH --time=10:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=2G 
+#SBATCH --mem=5G 
 
 # Submit job array
 #SBATCH --array=1-38%20
