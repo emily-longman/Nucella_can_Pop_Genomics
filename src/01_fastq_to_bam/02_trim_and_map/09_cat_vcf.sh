79972c17fc2fc4646c968ca234ca5a56bdfc8917
Emily Longman <Emily.Longman@uvm.edu>
2025-02-03 17:13:21 -0500

    Create 09_cat_vcf.sh

---
+++ src/01_fastq_to_bam/02_trim_and_map/09_cat_vcf.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=variant_calling_freebayes
+
+# Specify partition
+#SBATCH --partition=general
+
+# Request nodes
+#SBATCH --nodes=1 
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=2:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=5G 
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x.%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script will cat together the chunked vcf files generated in step 08 from freebayes.
+
+#--------------------------------------------------------------------------------
+
+# Define important file locations
+
+# WORKING_FOLDER is the core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Pop_Genomics/data/processed/fastq_to_bam
+
+#--------------------------------------------------------------------------------
+
+# Cat together the vcf files from all of the partitions
+
+# Set directory
+DIR=$WORKING_FOLDER/vcf_freebayes/partitions
+
+for file in $(find ${DIR} -name "*.vcf.gz"); do
+cmd="cat ${file};"
+eval $cmd
+done > $WORKING_FOLDER/vcf_freebayes/N_can_pops.vcf.gz
