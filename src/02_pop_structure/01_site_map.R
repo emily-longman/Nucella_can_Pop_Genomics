73b7eb3f4887891bfe28b1a7676b66af15f82d35
Emily Longman <Emily.Longman@uvm.edu>
2025-02-12 10:04:52 -0500

    Update 01_site_map.R

--- src/02_pop_structure/01_site_map.R
+++ src/02_pop_structure/01_site_map.R
@@ -18,11 +18,12 @@ setwd(output_path_from_root)
 # ================================================================================== #
 
 # Load packages
-install.packages(c('ggplot2', 'maps', 'mapdata', 'ggrepel'))
+install.packages(c('ggplot2', 'maps', 'mapdata', 'ggrepel', 'RColorBrewer'))
 library(ggplot2)
-library(maps) #Contains a lot of outlines of continents, countries, states, and counties
-library(mapdata) #Supplement to maps package
+library(maps) 
+library(mapdata)
 library(ggrepel)
+library(RColorBrewer)
 #library(ggsn) 
 
 # ================================================================================== #
@@ -36,44 +37,43 @@ west_coast <- subset(states, region %in% c("california", "oregon"))
 
 # Coordinates of sites 
 sites <- data.frame(
-  longitude = c(-123.074, -123.25506, -122.397576, -121.286767, -121.928989, -121.95367, -123.803572,
-                -124.114767, -124.084799, -124.0593, -124.401546, -124.564709, -124.252929, 
-                -124.080911, -123.78945, -121.31867, -120.63994, -120.61569, -120.88384),
-  latitude = c(38.319, 38.51198, 37.185064, 35.66549, 36.447503, 36.51939, 39.280903, 44.249988, 
-               44.505402, 44.83777, 43.304022, 42.840972, 41.771209, 40.030114, 39.604613, 35.72893,
-               34.88117, 34.73024, 35.28994),
-  site = c("BMR", "FR", "PGP", "PB", "SBR", "PL", "VD", "SH", "SLR", "FC", "ARA", "CBL", "PSG", "STC",
-           "KH", "PSN", "OCT", "STR", "HZD"), 
-  site.full = c("Bodega Marine Reserve", "Fort Ross", "Pigeon Point", "Piedras Blancas", 
-                "Soberanes Point", "Point Lobos", "Van Damme", "Strawberry Hill", 
-                "Seal Rock", "Fogarty Creek", "Cape Arago", "Cape Blanco", "Point Saint George", 
-                "Shelter Cove", "Kibesillah Hill", "Point Sierra Nevada", "Occulto", "Stairs", "Hazards")
+  longitude = c(-124.0593, -124.0848, -124.1148, -124.4015, -124.5647, -124.2529, -124.0809, -123.7895, -123.8036, 
+                -123.2551, -123.0740, -122.3976, -121.9537, -121.9290, -121.3187, -121.2868, -120.8838, -120.6399, -120.6157),
+  latitude = c(44.83777, 44.50540, 44.24999, 43.30402, 42.84097, 41.77121, 40.03011, 39.60461, 39.28090, 38.51198, 38.31900, 
+               37.18506, 36.51939, 36.44750, 35.72893, 35.66549, 35.28994, 34.88117, 34.73024),
+  site = c("FC", "SLR", "SH", "ARA", "CBL", "PSG", "STC", "KH", "VD", "FR", "BMR", "PGP", "PL", "SBR", "PSN", "PB", "HZD", "OCT", "STR"), 
+  site.full = c("Fogarty Creek"," Seal Rock",  "Strawberry Hill", "Cape Arago", "Cape Blanco",  "Point Saint George", "Shelter Cove", 
+                "Kibesillah Hill", "Van Damme", "Fort Ross", "Bodega Marine Reserve", "Pigeon Point", "Point Lobos", "Soberanes Point", 
+                "Point Sierra Nevada", "Piedras Blancas", "Hazards" , "Occulto", "Stairs")  
   )
 
 # Coordinates of site labels
-long.site.labels <- c(-123.074-0.75, -123.25506-0.6, -122.397576-0.75, -121.286767-0.75, 
-                      -121.928989-0.75, -121.95367-0.5, -123.803572-0.75, -124.114767-0.75,
-                      -124.084799-0.75, -124.0593-0.7, -124.401546-0.75, -124.564709-0.75,
-                      -124.252929-0.75, -124.080911-0.75, -123.78945-0.5, -121.31867-0.75, 
-                      -120.63994-0.75, -120.61569-0.6, -120.88384-0.7)
-long.site.labels.full <- c(-123.074-2.75, -123.25506-1.6, -122.397576-1.75, -121.286767-1.75, 
-                      -121.928989-1.9, -121.95367-1.5, -123.803572-1.75, -124.114767-1.75,
-                      -124.084799-1.75, -124.0593-1.7, -124.401546-1.5, -124.564709-1.5,
-                      -124.252929-2.25, -124.080911-1.75, -123.78945-1.5, -121.31867-2.75, 
-                      -120.63994-1.2, -120.61569-1.2, -120.88384-1.2)
-lat.site.labels <- c(38.319-0.05, 38.51198+0.04, 37.185064, 35.66549-0.07, 36.447503-0.09, 36.51939+0.1,
-                     39.280903, 44.249988-0.04, 44.505402, 44.83777+0.04, 43.304022, 42.840972, 
-                     41.771209, 40.030114, 39.604613-0.06, 35.72893+0.12, 34.88117, 
-                     34.73024-0.12, 35.2899409-0.02)
+long.site.labels <- c(-124.0593-0.7, -124.0848-0.75, -124.1148-0.75, -124.4015-0.75, -124.5647-0.75, -124.2529-0.75, 
+                      -124.0809-0.75, -123.7895-0.5, -123.8036-0.75, -123.2551-0.6, -123.0740-0.75, -122.3976-0.75, 
+                      -121.9537-0.5, -121.9290-0.75, -121.3187-0.75, -121.2868-0.75, -120.8838-0.7, -120.6399-0.75, -120.6157-0.6)
+
+long.site.labels.full <- c(-124.0593-1.7, -124.0848-1.75, -124.1148-1.75, -124.4015-1.5, -124.5647-1.5, -124.2529-2.25, 
+                      -124.0809-1.75, -123.7895-1.5, -123.8036-1.75, -123.2551-1.6, -123.0740-2.75, -122.3976-1.75, 
+                      -121.9537-1.5, -121.9290-1.9, -121.3187-2.75, -121.2868-1.75, -120.8838-1.2, -120.6399-1.2, -120.6157-1.2)
+
+lat.site.labels <- c(44.83777+0.04, 44.50540, 44.24999-0.04, 43.30402, 42.84097, 41.77121, 40.03011, 39.60461-0.06, 39.28090, 38.51198+0.04, 38.31900-0.05, 
+               37.18506, 36.51939+0.1, 36.44750-0.09, 35.72893+0.12, 35.66549-0.07, 35.28994-0.02, 34.88117, 34.73024-0.12)
+
+
+# ================================================================================== #
+
+# Color palette 
+nb.cols <- 19
+mycolors <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(nb.cols))
 
 # ================================================================================== #
 
 # Create site map (site codes)
-pdf("figures/Site_map.pdf", width = 10, height = 10)
-g<- ggplot(data = west_coast) + 
+pdf("figures/Site_map.pdf", width = 8, height = 8)
+g <- ggplot(data = west_coast) + 
   geom_polygon(aes(x = long, y = lat, group = group), fill = "white", color = "black") + 
-  geom_point(data = sites, aes(x = longitude, y = latitude), size = 4, 
-             shape = 20, fill = "black") + coord_fixed(1.3) +
+  geom_point(data = sites, aes(x = longitude, y = latitude), size = 5, 
+             shape = 21, fill = mycolors) + coord_fixed(1.3) +
   geom_text(data=sites, aes(long.site.labels, lat.site.labels, label=site))+ 
   xlim(c(-134, -114)) +
   xlab("Longitude") + ylab("Latitude") + theme_classic() 
@@ -82,11 +82,11 @@ g + annotate(geom = 'text', size = 5,
 dev.off()
 
 # Create site map (full site names)
-pdf("figures/Site_map_full_names.pdf", width = 10, height = 10)
-g<- ggplot(data = west_coast) + 
+pdf("figures/Site_map_full_names.pdf", width = 8, height = 8)
+g <- ggplot(data = west_coast) + 
   geom_polygon(aes(x = long, y = lat, group = group), fill = "white", color = "black") + 
   geom_point(data = sites, aes(x = longitude, y = latitude), size = 4, 
-             shape = 20, fill = "black") + coord_fixed(1.3) +
+             shape = 21, fill = mycolors) + coord_fixed(1.3) +
   geom_text(data=sites, aes(long.site.labels.full, lat.site.labels, label=site.full))+ 
   xlim(c(-134, -114)) +
   xlab("Longitude") + ylab("Latitude") + theme_classic()
