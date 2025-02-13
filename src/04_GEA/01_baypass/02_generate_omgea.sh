53f53d469054157a8457661ce6a309d3f4495550
Emily Longman <Emily.Longman@uvm.edu>
2025-02-13 13:38:32 -0500

    Update baypass scripts

--- src/04_GEA/01_baypass/02_generate_omgea.sh
+++ src/04_GEA/01_baypass/02_generate_omgea.sh
@@ -5,7 +5,7 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=reformat_VCF
+#SBATCH --job-name=baypass_omega
 
 # Specify partition
 #SBATCH --partition=general
@@ -14,10 +14,13 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=10:00:00 
+#SBATCH --time=30:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=8G 
+#SBATCH --mem=40G 
+
+# Request CPU
+#SBATCH --cpus-per-task=5
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%j.out # Standard output
@@ -28,10 +31,11 @@
 
 #--------------------------------------------------------------------------------
 
-# This script will use plink to reformat the vcf so that it is in bed format.
+# This script will perform the first step in running baypass. 
 
-# Load software
-plink=/gpfs1/home/e/l/elongman/software/plink_linux_x86_64_20241022/plink
+# Load modules 
+module load gcc/10.5.0
+baypass=/gpfs1/home/e/l/elongman/software/baypass_public/sources/g_baypass
 
 #--------------------------------------------------------------------------------
 
@@ -42,16 +46,20 @@ WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Pop_Genomics/data/processed
 
 #--------------------------------------------------------------------------------
 
-# Generate Folders and files
+# Change directory 
+cd $WORKING_FOLDER/GEA/baypass/omega_file
 
-# Move to working directory
-cd $WORKING_FOLDER/outlier_analyses
+# Run baypass - this will generate the omega file which will be used in the subsequent scripts
+$baypass -npop 19 \
+-gfile $WORKING_FOLDER/GEA/baypass/genobaypass \
+-poolsizefile $WORKING_FOLDER/GEA/baypass/poolsize \
+-d0yij 8 \
+-outprefix NC_baypass \
+-npilot 100 -nthreads 5
 
-# This part of the script will check and generate, if necessary, all of the output folders used in the script
-
-if [ -d "pcadapt" ]
-then echo "Working pcadapt folder exist"; echo "Let's move on."; date
-else echo "Working pcadapt folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/outlier_analyses/pcadapt; date
-fi
-
-#--------------------------------------------------------------------------------
+#-npop: number of pools
+#-gfile: gfile input
+#-poolsizefile: poolsize file
+#-d0yij = is something to do with the initial read counts and their distribution seeded into the model, and is specifically for Pool-seq data. I followed the tip in the manual that says to set it to 1/5th of the minimum pool size
+#-outprefix = the header of the output files
+#-npilot = number of pilot runs
