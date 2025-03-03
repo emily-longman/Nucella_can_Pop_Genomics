a00f034d1b6b86d45d3871c7ee1536bc2d3791e9
Emily Longman <Emily.Longman@uvm.edu>
2025-03-03 09:42:35 -0500

    Update baypass scripts

---
+++ src/02_pop_structure/05_grenedalf.R
@@ -0,0 +1,84 @@
+# Calculate Fst statistics and gnerate PCA of all SNPs
+
+# Clear memory
+rm(list=ls()) 
+
+# ================================================================================== #
+
+# Set path as main Github repo
+install.packages(c('rprojroot'))
+library(rprojroot)
+
+# List all files and directories below the root
+dir(find_root_file(criterion = has_file("README.md")))
+root_path <- find_root_file(criterion = has_file("README.md"))
+# Set working directory as path from root
+setwd(root_path)
+
+# ================================================================================== #
+
+# Load packages
+install.packages(c('ggplot2', 'RColorBrewer'))
+library(ggplot2)
+library(RColorBrewer)
+
+# ================================================================================== #
+
+# Load data
+meta <- read.csv("data/processed/pop_gen/guide_files/Populations_metadata.csv", header=T)
+grenedalf <- read.csv("data/processed/pop_gen/grenedalf/diversity.csv", header=F)
+
+# ================================================================================== #
+
+# Color palette 
+nb.cols <- 19
+mycolors <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(nb.cols))
+colors.alphabetical <- mycolors[c(4,11,5,1,10,17,8,18,16,12,13,6,15,14,3,2,7,19,9)]
+
+# ================================================================================== #
+
+# Extract and reformat data for theta pi
+theta_pi <- t(grenedalf[, grep("theta_pi", grenedalf[1,])])
+theta_pi_names <- data.frame(do.call('rbind', strsplit(as.character(theta_pi[,1]),'.',fixed=TRUE)))
+theta_pi <- data.frame(theta_pi_names[,1], as.numeric(theta_pi[,2]))
+colnames(theta_pi) <- c("Site", "theta_pi")
+theta_pi <- merge(theta_pi, meta, by="Site")
+
+pdf("output/figures/pop_structure/Theta_pi.pdf", width = 5, height = 5)
+ggplot(data = theta_pi, aes(x = Lat, y = theta_pi)) + 
+  geom_point(shape = 21, size = 3, fill = colors.alphabetical) + 
+  xlab("Latitude") + ylab("Theta pi") + theme_classic() 
+dev.off()
+
+# ================================================================================== #
+
+# Extract and reformat data for theta watterson
+theta_watterson <- t(grenedalf[, grep("theta_watterson", grenedalf[1,])])
+theta_watterson_names <- data.frame(do.call('rbind', strsplit(as.character(theta_watterson[,1]),'.',fixed=TRUE)))
+theta_watterson <- data.frame(theta_watterson_names[,1], as.numeric(theta_watterson[,2]))
+colnames(theta_watterson) <- c("Site", "theta_watterson")
+theta_watterson <- merge(theta_watterson, meta, by="Site")
+
+pdf("output/figures/pop_structure/Theta_watterson.pdf", width = 5, height = 5)
+ggplot(data = theta_watterson, aes(x = Lat, y = theta_watterson)) + 
+  geom_point(shape = 21, size = 3, fill = colors.alphabetical) + 
+  xlab("Latitude") + ylab("Theta Watterson") + theme_classic() 
+dev.off()
+
+
+# ================================================================================== #
+
+# Extract and reformat data for Tajima D
+tajimas_d <- t(grenedalf[, grep("tajimas_d", grenedalf[1,])])
+tajimas_d_names <- data.frame(do.call('rbind', strsplit(as.character(tajimas_d[,1]),'.',fixed=TRUE)))
+tajimas_d <- data.frame(tajimas_d_names[,1], as.numeric(tajimas_d[,2]))
+colnames(tajimas_d) <- c("Site", "tajimas_d")
+tajimas_d <- merge(tajimas_d, meta, by="Site")
+
+pdf("output/figures/pop_structure/Tajimas_d.pdf", width = 5, height = 5)
+ggplot(data = tajimas_d, aes(x = Lat, y = tajimas_d)) + 
+  geom_point(shape = 21, size = 3, fill = colors.alphabetical) + 
+  xlab("Latitude") + ylab("Tajima's d") + theme_classic() 
+dev.off()
+
+# A negative Tajima's D signifies an excess of low frequency polymorphisms relative to expectation, indicating population size expansion
--- src/03_GEA/02_baypass/04_baypass_xtx.sh
+++ src/03_GEA/02_baypass/04_baypass_xtx.sh
@@ -5,22 +5,22 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=baypass_omega
+#SBATCH --job-name=baypass_xtx
 
 # Specify partition
-#SBATCH --partition=week
+#SBATCH --partition=general
 
 # Request nodes
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=7-00:00:00 
+#SBATCH --time=30:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=60G 
 
 # Request CPU
-#SBATCH --cpus-per-task=25
+#SBATCH --cpus-per-task=20
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%j.out # Standard output
@@ -31,11 +31,10 @@
 
 #--------------------------------------------------------------------------------
 
-# This script will perform the first step in running baypass. 
+# This script will run baypass in core model (i.e., xtx mode) using an omega file. 
 
 # Load modules 
 module load gcc/13.3.0-xp3epyt
-#module load netcdf-fortran/4.6.1 # Use to compile baypass
 baypass=/gpfs1/home/e/l/elongman/software/baypass_public/sources/g_baypass
 
 #--------------------------------------------------------------------------------
@@ -53,27 +52,22 @@ WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Pop_Genomics/data/processed
 cd $WORKING_FOLDER/GEA/baypass
 
 # This part of the script will check and generate, if necessary, all of the output folders used in the script
-if [ -d "omega" ]
-then echo "Working omega folder exist"; echo "Let's move on."; date
-else echo "Working omega folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/GEA/baypass/omega; date
+if [ -d "xtx" ]
+then echo "Working xtx folder exist"; echo "Let's move on."; date
+else echo "Working xtx folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/GEA/baypass/xtx; date
 fi
 
 #--------------------------------------------------------------------------------
 
 # Change directory 
-cd $WORKING_FOLDER/GEA/baypass/omega
+cd $WORKING_FOLDER/GEA/baypass/xtx
 
-# Run baypass - this will generate the omega file which will be used in the subsequent scripts
+# Run baypass in xtx mode
 $baypass -npop 19 \
 -gfile $WORKING_FOLDER/GEA/baypass/genobaypass \
 -poolsizefile $WORKING_FOLDER/GEA/baypass/poolsize \
+-omega $WORKING_FOLDER/GEA/baypass/omega/NC_baypass_mat_omega.out\
 -d0yij 4 \
--outprefix NC_baypass \
--npilot 100 -nthreads 25
+-outprefix NC_baypass_core \
+-nthreads 20
 
-#-npop: number of pools
-#-gfile: gfile input
-#-poolsizefile: poolsize file
-#-d0yij = Initial delta for the yij (for pool-seq mode). The value is eventually updated for each locus and pop during the pilot runs.  Recommended to set to 1/5th the minimum pool size
-#-outprefix = the header of the output files
-#-npilot = number of pilot runs
