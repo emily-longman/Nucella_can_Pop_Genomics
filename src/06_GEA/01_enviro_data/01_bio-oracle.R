c030ff07daaa6eb2a8367361dd9c6c4a2ba66983
Emily Longman <Emily.Longman@uvm.edu>
2025-05-07 10:05:39 -0400

    Create baypass abiotic

--- src/06_GEA/01_enviro_data/01_bio-oracle.R
+++ src/06_GEA/01_enviro_data/01_bio-oracle.R
@@ -1,7 +1,7 @@
 # Download Bio-Oracle data (https://www.bio-oracle.org/index.php )
 # Use R package biooracler to access the Bio-Oracle dataset via ERDDAP server (https://github.com/bio-oracle/biooracler)
 
-# Note: need to run this script on my computer rather than the VACC becuase incompatibilities with R version and some of the programs
+# Note: need to run this script on my computer rather than the VACC because incompatibilities with R version and some of the programs
 
 # Clear memory
 rm(list=ls()) 
@@ -17,6 +17,9 @@ dir(find_root_file(criterion = has_file("README.md")))
 raw_data_path_from_root <- find_root_file("data", "raw", "Bio-oracle", criterion = has_file("README.md"))
 # Set working directory as path from root
 setwd(raw_data_path_from_root)
+
+# Specify output directory
+out_dir <- "/Users/emilylongman/Documents/GitHub/Nucella_can_Pop_Genomics/data/raw/Bio-oracle"
 
 # ================================================================================== #
 
@@ -36,7 +39,12 @@ list_layers()
 # See all layers for a specific variable (e.g., temp)
 print(list_layers("temp"), n=100)
 
-# Define time, lat, and long  
+# ================================================================================== #
+# ================================================================================== #
+
+# Extract present day data for range of sites
+
+# Define time, lat, and long (set lat and long to fully encompass all sites)
 time = c('2000-01-01T00:00:00Z', '2010-01-01T00:00:00Z')
 latitude = c(34, 45)
 longitude = c(-120, -125)
@@ -45,8 +53,9 @@ longitude = c(-120, -125)
 constraints = list(time, latitude, longitude)
 names(constraints) = c("time", "latitude", "longitude")
 
-# Specify output directory
-dir <- "/Users/emilylongman/Documents/GitHub/Nucella_can_Pop_Genomics/data/raw/Bio-oracle"
+# ================================================================================== #
+
+# Present day data
 
 # Set dataset IDs
 SST_Present <-"thetao_baseline_2000_2019_depthsurf"
@@ -55,30 +64,18 @@ Chl_Present <-"chl_baseline_2000_2018_depthsurf"
 O2_Present<-"o2_baseline_2000_2018_depthsurf"
 sal_Present<-"so_baseline_2000_2019_depthsurf"
 
-
 # Specify datasets and summary statistics to download 
 datasets <- list(
-  list(dataset_id = SST_Present,
-       variables = c("thetao_max","thetao_min", "thetao_range", "thetao_mean"),
-       constraints = constraints,
-       fmt = "csv", directory = dir),
-  list(dataset_id = pH_Present,
-       variables = c("ph_min", "ph_mean"),
-       constraints = constraints,
-       fmt = "csv", directory = dir),
-  list(dataset_id = Chl_Present,
-       variables = c("chl_mean"),
-       constraints = constraints,
-       fmt = "csv", directory = dir),
-  list(dataset_id = O2_Present,
-       variables = c("o2_mean"),
-       constraints = constraints,
-       fmt = "csv", directory = dir),
-  list(dataset_id = sal_Present,
-       variables = c("so_mean"),
-       constraints = constraints,
-       fmt = "csv", directory = dir)
-)
+  list(dataset_id = SST_Present, variables = c("thetao_max","thetao_min", "thetao_range", "thetao_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = pH_Present, variables = c("ph_min", "ph_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = Chl_Present, variables = c("chl_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = O2_Present, variables = c("o2_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = sal_Present, variables = c("so_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir))
 
 
 # Download datasets using for loop
@@ -89,19 +86,17 @@ for (dataset in datasets) {
   constraints <- dataset$constraints
   
   # List files before downloading
-  files_before <- list.files(dir, full.names = TRUE)
-  
+  files_before <- list.files(out_dir, full.names = TRUE)
   # Download dataset
-  download_layers(dataset_id, variables = variables, constraints = constraints, fmt = "csv", directory = dir)
-  
+  download_layers(dataset_id, variables = variables, constraints = constraints, fmt = "csv", directory = out_dir)
   # List files after downloading
-  files_after <- list.files(dir, full.names = TRUE)
-  
+  files_after <- list.files(out_dir, full.names = TRUE)
   # Identify the newly downloaded file
   new_file <- setdiff(files_after, files_before)
   
+  # Rename the file to match the dataset ID
   if (length(new_file) == 1) {
-    new_name <- file.path(dir, paste0(dataset_id, ".csv"))
+    new_name <- file.path(out_dir, paste0(dataset_id, ".csv"))
     file.rename(new_file, new_name)
     message("Rename ", new_file, " to ", new_name)
   } else {
@@ -109,7 +104,80 @@ for (dataset in datasets) {
   }
 }
 
-# Check list of files
-list.files(dir)
+
+# Check list of files in output directory
+list.files(out_dir)
+
+# Note: upload these files to the appropriate folder on the VACC
+
+# ================================================================================== #
+# ================================================================================== #
+
+
+# Extract present day data for range of sites
+
+# Define time, lat, and long (set lat and long to fully encompass all sites)
+time = c('2080-01-01T00:00:00Z', '2090-01-01T00:00:00Z')
+latitude = c(34, 45)
+longitude = c(-120, -125)
+
+# Set constraints
+constraints = list(time, latitude, longitude)
+names(constraints) = c("time", "latitude", "longitude")
+
+# ================================================================================== #
+
+# Future data
+
+# Set dataset IDs
+SST_SSP585 <- "thetao_ssp585_2020_2100_depthsurf"
+pH_SSP585 <- "ph_ssp585_2020_2100_depthsurf"
+Chl_SSP585 <- "chl_ssp585_2020_2100_depthsurf"
+O2_SSP585 <- "o2_ssp585_2020_2100_depthsurf"
+sal_SSP585 <- "so_ssp585_2020_2100_depthsurf"
+
+# Specify datasets and summary statistics to download 
+datasets <- list(
+  list(dataset_id = SST_SSP585, variables = c("thetao_max","thetao_min", "thetao_range", "thetao_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = pH_SSP585, variables = c("ph_min", "ph_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = Chl_SSP585, variables = c("chl_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = O2_SSP585, variables = c("o2_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir),
+  list(dataset_id = sal_SSP585, variables = c("so_mean"),
+       constraints = constraints, fmt = "csv", directory = out_dir))
+
+
+# Download datasets using for loop
+for (dataset in datasets) {
+  
+  dataset_id <- dataset$dataset_id
+  variables <- dataset$variables
+  constraints <- dataset$constraints
+  
+  # List files before downloading
+  files_before <- list.files(out_dir, full.names = TRUE)
+  # Download dataset
+  download_layers(dataset_id, variables = variables, constraints = constraints, fmt = "csv", directory = out_dir)
+  # List files after downloading
+  files_after <- list.files(out_dir, full.names = TRUE)
+  # Identify the newly downloaded file
+  new_file <- setdiff(files_after, files_before)
+  
+  # Rename the file to match the dataset ID
+  if (length(new_file) == 1) {
+    new_name <- file.path(out_dir, paste0(dataset_id, ".csv"))
+    file.rename(new_file, new_name)
+    message("Rename ", new_file, " to ", new_name)
+  } else {
+    message("No new file found for ", dataset_id, " or multiple new files detected.")
+  }
+}
+
+
+# Check list of files in output directory
+list.files(out_dir)
 
 # Note: upload these files to the appropriate folder on the VACC
--- src/06_GEA/01_enviro_data/02_format_bio-oracle.R
+++ src/06_GEA/01_enviro_data/02_format_bio-oracle.R
@@ -1,4 +1,4 @@
-# Format Bio-Oracle data (https://www.bio-oracle.org/index.php )
+# Format Bio-Oracle data (https://www.bio-oracle.org/index.php)
 
 # Clear memory
 rm(list=ls()) 
@@ -18,64 +18,130 @@ setwd(root_path)
 # ================================================================================== #
 
 # Load packages
-install.packages(c('purr', 'ggplot2', 'RColorBrewer'))
-library(purr)
+install.packages(c('data.table', 'tidyverse', 'ggplot2', 'RColorBrewer'))
+library(data.table)
+library(tidyverse)
 library(ggplot2)
 library(RColorBrewer)
 
 # ================================================================================== #
-
-# Load site metadata
-meta <- read.csv("data/processed/GEA/guide_files/Populations_metadata.csv", header=T)
-
-# Read in Bio-oracle data
-sst <- read.csv("data/raw/Bio-oracle/thetao_baseline_2000_2019_depthsurf.csv", header=T)
-chl <- read.csv("data/raw/Bio-oracle/chl_baseline_2000_2018_depthsurf.csv", header=T)
-o2 <- read.csv("data/raw/Bio-oracle/o2_baseline_2000_2018_depthsurf.csv", header=T)
-ph <- read.csv("data/raw/Bio-oracle/ph_baseline_2000_2018_depthsurf.csv", header=T)
-so <- read.csv("data/raw/Bio-oracle/so_baseline_2000_2019_depthsurf.csv", header=T)
-
 # ================================================================================== #
 
-# Population names
-pops <- meta$Site
-sites <- cbind("longitude" = meta$long, "latitude" =  meta$Lat)
+# Present day data
+
+# Read in Bio-oracle data
+sst <- read.csv("data/raw/Bio-oracle/present/thetao_baseline_2000_2019_depthsurf.csv", header=T)
+chl <- read.csv("data/raw/Bio-oracle/present/chl_baseline_2000_2018_depthsurf.csv", header=T)
+o2 <- read.csv("data/raw/Bio-oracle/present/o2_baseline_2000_2018_depthsurf.csv", header=T)
+ph <- read.csv("data/raw/Bio-oracle/present/ph_baseline_2000_2018_depthsurf.csv", header=T)
+so <- read.csv("data/raw/Bio-oracle/present/so_baseline_2000_2019_depthsurf.csv", header=T)
+
+# ================================================================================== #
 
 # Combine datasets into one large dataset
 bio_oracle <- reduce(list(sst, chl, o2, ph, so), full_join, by = c("time", "latitude", "longitude"))
 
+# Remove first row (i.e. units)
+bio_oracle <- bio_oracle[-1,]
 
-# Extract site specific data
-sites_environ <- data.frame(Name=sites, extract(bio_oracle, sites))
+# Change latitude and longitude to numeric
+bio_oracle <- bio_oracle %>%
+  mutate(latitude = as.numeric(latitude), longitude = as.numeric(longitude))
 
-allpresentdata <- combined_data %>%
-  mutate(
-    latitude = as.numeric(latitude),
-    longitude = as.numeric(longitude),
-    location = case_when(
-      latitude == 43.304 & longitude == -124.402 ~ "ARA",
-      latitude == 38.319 & longitude == -123.074 ~ "BMR",
-      latitude == 42.841 & longitude == -124.565 ~ "CBL",
-      latitude == 44.838 & longitude == -124.059 ~ "FC",
-      latitude == 38.512 & longitude == -123.255 ~ "FR",
-      latitude == 35.290 & longitude == -120.884 ~ "HZD",
-      latitude == 39.605 & longitude == -123.789 ~ "KH",
-      latitude == 34.881 & longitude == -120.640 ~ "OCT",
-      latitude == 35.665 & longitude == -121.287 ~ "PB",
-      latitude == 37.185 & longitude == -122.398 ~ "PGP",
-      latitude == 36.519 & longitude == -121.954 ~ "PL",
-      latitude == 41.771 & longitude == -124.253 ~ "PSG",
-      latitude == 35.729 & longitude == -121.319 ~ "PSN",
-      latitude == 36.448 & longitude == -121.929 ~ "SBR",
-      latitude == 44.250 & longitude == -124.115 ~ "SH",
-      latitude == 44.505 & longitude == -124.085 ~ "SLR",
-      latitude == 40.030 & longitude == -124.081 ~ "STC",
-      latitude == 34.730 & longitude == -120.616 ~ "STR",
-      latitude == 39.281 & longitude == -123.804 ~ "VD",
+# Filter bio-oracle data for the 19 sites (will set NA for any lat long combo not specified, then will filter out those rows)
+# Note: latitude and longitude need to be rounded to nearest 0.025 or 0.075
+# Note: during rounding make sure you don't set the location as a spot on land
+bio_oracle_sites <- bio_oracle %>%
+  mutate(location = case_when(
+      latitude == 43.325 & longitude == -124.425 ~ "ARA",
+      latitude == 38.325 & longitude == -123.075 ~ "BMR",
+      latitude == 42.825 & longitude == -124.575 ~ "CBL",
+      latitude == 44.825 & longitude == -124.075 ~ "FC",
+      latitude == 38.525 & longitude == -123.275 ~ "FR",
+      latitude == 35.275 & longitude == -120.925 ~ "HZD",
+      latitude == 39.625 & longitude == -123.825 ~ "KH",
+      latitude == 34.875 & longitude == -120.675 ~ "OCT",
+      latitude == 35.675 & longitude == -121.325 ~ "PB",
+      latitude == 37.175 & longitude == -122.375 ~ "PGP",
+      latitude == 36.525 & longitude == -121.975 ~ "PL",
+      latitude == 41.775 & longitude == -124.275 ~ "PSG",
+      latitude == 35.725 & longitude == -121.325 ~ "PSN",
+      latitude == 36.425 & longitude == -121.925 ~ "SBR",
+      latitude == 44.225 & longitude == -124.125 ~ "SH",
+      latitude == 44.525 & longitude == -124.075 ~ "SLR",
+      latitude == 40.025 & longitude == -124.075 ~ "STC",
+      latitude == 34.725 & longitude == -120.625 ~ "STR",
+      latitude == 39.275 & longitude == -123.825 ~ "VD",
       TRUE ~ NA_character_
     )
   ) %>%
   filter(!is.na(location))
 
-allpresentdata
+# Check structure
+str(bio_oracle_sites)
 
+# ================================================================================== #
+
+# Write table
+write.csv(bio_oracle_sites, "data/processed/GEA/data/bio_oracle_sites.csv")
+
+# ================================================================================== #
+# ================================================================================== #
+
+# Future data
+
+# Read in Bio-oracle data
+sst_ssp585 <- read.csv("data/raw/Bio-oracle/future/thetao_ssp585_2020_2100_depthsurf.csv", header=T)
+chl_ssp585 <- read.csv("data/raw/Bio-oracle/future/chl_ssp585_2020_2100_depthsurf.csv", header=T)
+o2_ssp585 <- read.csv("data/raw/Bio-oracle/future/o2_ssp585_2020_2100_depthsurf.csv", header=T)
+ph_ssp585 <- read.csv("data/raw/Bio-oracle/future/ph_ssp585_2020_2100_depthsurf.csv", header=T)
+so_ssp585 <- read.csv("data/raw/Bio-oracle/future/so_ssp585_2020_2100_depthsurf.csv", header=T)
+
+# ================================================================================== #
+
+# Combine datasets into one large dataset
+bio_oracle_ssp585 <- reduce(list(sst_ssp585, chl_ssp585, o2_ssp585, ph_ssp585, so_ssp585), full_join, by = c("time", "latitude", "longitude"))
+
+# Remove first row (i.e. units)
+bio_oracle_ssp585 <- bio_oracle_ssp585[-1,]
+
+# Change latitude and longitude to numeric
+bio_oracle_ssp585 <- bio_oracle_ssp585 %>%
+  mutate(latitude = as.numeric(latitude), longitude = as.numeric(longitude))
+
+# Filter bio-oracle data for the 19 sites (will set NA for any lat long combo not specified, then will filter out those rows)
+# Note: latitude and longitude need to be rounded to nearest 0.025 or 0.075
+# Note: during rounding make sure you don't set the location as a spot on land
+bio_oracle_ssp585_sites <- bio_oracle_ssp585 %>%
+  mutate(location = case_when(
+      latitude == 43.325 & longitude == -124.425 ~ "ARA",
+      latitude == 38.325 & longitude == -123.075 ~ "BMR",
+      latitude == 42.825 & longitude == -124.575 ~ "CBL",
+      latitude == 44.825 & longitude == -124.075 ~ "FC",
+      latitude == 38.525 & longitude == -123.275 ~ "FR",
+      latitude == 35.275 & longitude == -120.925 ~ "HZD",
+      latitude == 39.625 & longitude == -123.825 ~ "KH",
+      latitude == 34.875 & longitude == -120.675 ~ "OCT",
+      latitude == 35.675 & longitude == -121.325 ~ "PB",
+      latitude == 37.175 & longitude == -122.375 ~ "PGP",
+      latitude == 36.525 & longitude == -121.975 ~ "PL",
+      latitude == 41.775 & longitude == -124.275 ~ "PSG",
+      latitude == 35.725 & longitude == -121.325 ~ "PSN",
+      latitude == 36.425 & longitude == -121.925 ~ "SBR",
+      latitude == 44.225 & longitude == -124.125 ~ "SH",
+      latitude == 44.525 & longitude == -124.075 ~ "SLR",
+      latitude == 40.025 & longitude == -124.075 ~ "STC",
+      latitude == 34.725 & longitude == -120.625 ~ "STR",
+      latitude == 39.275 & longitude == -123.825 ~ "VD",
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(location))
+
+# Check structure
+str(bio_oracle_ssp585_sites)
+
+# ================================================================================== #
+
+# Write table
+write.csv(bio_oracle_ssp585_sites, "data/processed/GEA/data/bio_oracle_ssp585_sites.csv")
