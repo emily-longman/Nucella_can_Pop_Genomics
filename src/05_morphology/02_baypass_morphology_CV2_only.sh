dcb03c3cb0fde3086a6e3ab40e12351fb6c24584
Emily Longman <Emily.Longman@uvm.edu>
2025-05-21 09:03:34 -0400

    Create baypass CV1 and CV2 only scripts

---
+++ src/05_morphology/02_baypass_morphology_CV2_only.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=baypass_morphology_CV_2
+
+# Specify partition
+#SBATCH --partition=week
+
+# Request nodes
+#SBATCH --nodes=1 
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=3-00:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=40G 
+
+# Request CPU
+#SBATCH --cpus-per-task=20
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x.%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script will run baypass using PC1 and PC2 of the morphology shape data and using an omega file. 
+
+# Load modules 
+module load gcc/13.3.0-xp3epyt
+baypass=/gpfs1/home/e/l/elongman/software/baypass_public/sources/g_baypass
+
+#--------------------------------------------------------------------------------
+
+# Define important file locations
+
+# WORKING_FOLDER is the core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Pop_Genomics/data/processed
+
+#--------------------------------------------------------------------------------
+
+# Generate Folders and files
+
+# Move to working directory
+cd $WORKING_FOLDER/morphometrics
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+if [ -d "baypass" ]
+then echo "Working baypass folder exist"; echo "Let's move on."; date
+else echo "Working baypass folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/morphometrics/baypass; date
+fi
+
+# Change directory 
+cd $WORKING_FOLDER/morphometrics/baypass
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+if [ -d "CV_2" ]
+then echo "Working CV_2 folder exist"; echo "Let's move on."; date
+else echo "Working CV_2 folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/morphometrics/baypass/CV_2; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Change directory 
+cd $WORKING_FOLDER/morphometrics/baypass/CV_2
+
+# Run baypass with morphometric data
+$baypass -npop 19 \
+-gfile $WORKING_FOLDER/outlier_analyses/baypass/genobaypass \
+-poolsizefile $WORKING_FOLDER/outlier_analyses/baypass/poolsize \
+-omegafile $WORKING_FOLDER/outlier_analyses/baypass/omega/NC_baypass_mat_omega.out \
+-efile $WORKING_FOLDER/morphometrics/guide_files/Baypass_morphology_CV_2.txt \
+-d0yij 4 \
+-outprefix NC_baypass_morphology_CV_2 \
+-nthreads 20
+
