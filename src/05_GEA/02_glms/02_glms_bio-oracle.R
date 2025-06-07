c3fd130dbc494985013e7ef850720ebc8dfd7569
Emily Longman <Emily.Longman@uvm.edu>
2025-06-07 09:27:07 -0400

    Update glm scripts

--- src/05_GEA/02_glms/02_glms_bio-oracle.R
+++ src/05_GEA/02_glms/02_glms_bio-oracle.R
@@ -39,7 +39,7 @@ library(SeqArray)
 
 # Specify arguments
 args = commandArgs(trailingOnly=TRUE)
-y = as.numeric(args[1]) # Chunk: this is the chunk of 1000 windows  (total of 32 chunks)
+c = as.numeric(args[1]) # Chunk: this is the chunk of 1000 windows  (total of 32 chunks)
 k = as.numeric(args[2]) # Array: this is a specific window within a given chunk (1000 windows in each chunk)
 
 # ================================================================================== #
@@ -65,9 +65,8 @@ load("data/processed/GEA/glms/glms_windows.RData")
 # Rename "location" column as "sampleId"
 names(bio_oracle_sites_2010)[names(bio_oracle_sites_2010) == "location"] <- "sampleId"
 
-# Create demography column based on genetic clustering
-bio_oracle_sites_2010$demography <- 
-ifelse(bio_oracle_sites_2010$sampleId == "PL" | bio_oracle_sites_2010$sampleId == "SBR", "Admix",
+# Create demography column based on genetic clustering - North, South and Admix
+bio_oracle_sites_2010$demography <- ifelse(bio_oracle_sites_2010$sampleId == "PL" | bio_oracle_sites_2010$sampleId == "SBR", "Admix", 
 ifelse(bio_oracle_sites_2010$latitude >= 36.8, "North", "South"))
 
 # Create SNP_id column for outlier SNP list
@@ -118,30 +117,30 @@ length(subdivision_list)
 # ================================================================================== #
 
 # Get data for a specified window
-# To do so, specify a given chunk ('y'), then a given window ('k' array)
-message(paste("I am doing chunk number y =", y, "and array number k =", k,  sep = " "))
 
-# Get window list for chunk y
-chunk_of_choice <- subdivision_list[[y]]
+# To do so, specify a given chunk ('c'), then a given window ('k' array)
+message(paste("I am doing chunk number c =", c, "and array number k =", k,  sep = " "))
+
+# Get window list for chunk c
+chunk_of_choice <- subdivision_list[[c]]
 
 # Check number of elements/windows in chunk
-message(paste("Chunk", y, "has", length(chunk_of_choice), "windows",  sep = " "))
+message(paste("Chunk", c, "has", length(chunk_of_choice), "windows",  sep = " "))
 
-# Get window information for chunk 'y'
-wins %>%
-filter(i %in% chunk_of_choice) -> 
-wins.y
+# Get windows for chunk 'c'
+wins %>% filter(i %in% chunk_of_choice) -> wins.c
 
-# Filter snp.dt for a given window "k" in chunk "y"
+# Filter snp.dt for a given window "k" in chunk "c" - i.e., identify all of the sig SNPs in a given window
 snp.dt %>%
-filter(chr == wins.y$chr[k]) %>%
-filter(pos > wins.y$start[k] & pos < wins.y$end[k]) -> 
+filter(chr == wins.c$chr[k]) %>%
+filter(pos > wins.c$start[k] & pos < wins.c$end[k]) -> 
 data_win
 
 # ================================================================================== #
 
-model.output =
-  # For each SNP in a given window extract allele freq then run model with bio-oracle data
+glm.model.output =
+
+  # For each SNP in a given window k extract allele freq then run model with bio-oracle data
   foreach(i=1:dim(data_win)[1], .combine = "rbind")%do%{
     
     # Reset filter
@@ -149,7 +148,7 @@ model.output =
 
     ###############################################################
 
-    # Calculate allele frequency for SNP i in window k in chunk y
+    # Calculate allele frequency for SNP i in window k in chunk c
     seqSetFilter(genofile, variant.id=data_win$variant.id[i], verbose = T)
 
       # Extract allele depth ('ad') of alternate allele for SNP i
@@ -157,7 +156,7 @@ model.output =
       # Extract total depth ('dp') for SNP i
       dp_i <- seqGetData(genofile, "annotation/format/DP")[,1]
 
-      # Create af data table with ad, dp, sample ID and variant id for SNP i
+      # Create allele freq (af) data table with ad, dp, af, sample ID and variant id for SNP i
       af_i <- data.table(ad=ad_i, dp=dp_i, af=ad_i/dp_i,
       sampleId=seqGetData(genofile, "sample.id"),
       variant.id=rep(seqGetData(genofile, "variant.id"), each=length(ad_i)))
@@ -165,8 +164,9 @@ model.output =
       # Merge allele freq table af_i and snp.dt
       af_i_snp <- merge(af_i, snp.dt, by="variant.id")
 
-      # Calculate the mean effective coverage ('nEff') (note: each pool consists of 20 dogwhelks)
+      # Sample size of each pool
       nSnail=20
+      # Calculate the mean effective coverage ('nEff') (note: each pool consists of 20 dogwhelks)
       af_i_snp[,nEff:=round((dp*2*nSnail)/(2*nSnail+dp-1))]
       # Calculate the effective allele freq
       af_i_snp[,af_nEff:=round(af*nEff)/nEff]
@@ -176,16 +176,13 @@ model.output =
     # Join with bio-oracle environmental data
     left_join(af_i_snp, bio_oracle_sites_2010, by ="sampleId") -> af_i_snp_enviro
     
-    # Create long format data table with the enviro data in column "value" and the specific variable identified in column "column"
+    # Create long format data table with the enviro data in column labeled "value" and the specific variable identified in the column "column"
     af_i_snp_enviro %>% as_tibble %>% gather(key = "enviro_var", value = "value", `thetao_max`:`so_mean`) -> gathered_data
     
     # Get names of environmental variables
     unique(gathered_data$enviro_var) -> enviro_vars_names
     
     ###############################################################
-
-    # Null t0 model
-    t0 <- lm(af~1, data=gathered_data)
 
     # Run model for each variable
     real_estimates =
@@ -194,16 +191,17 @@ model.output =
         # Extract data for 'j' environmental variable
         gathered_data %>% filter(enviro_var == j) -> inner.tmp
         
-        # Model allele freq for 'j' enviro variable
+        # Model allele freq
+        # Generate 3 models - a null model (t0), a model with just demography (t1.dem) and a model with demography and "j" enviro var (t1.dem.env)
         y <- inner.tmp$af_nEff
         X.null <- model.matrix(~1, inner.tmp)
         X.dem <- model.matrix(~as.factor(demography), inner.tmp)
         X.dem.env <- model.matrix(~as.factor(demography)+value, inner.tmp)
         t0 <- fastglm(x=X.null, y=y, family=binomial(), weights=inner.tmp$nEff, method=0)
-        t1.demo <- fastglm(x=X.dem, y=y, family=binomial(), weights=inner.tmp$nEff, method=0)
+        t1.dem <- fastglm(x=X.dem, y=y, family=binomial(), weights=inner.tmp$nEff, method=0)
         t1.dem.env <- fastglm(x=X.dem.env, y=y, family=binomial(), weights=inner.tmp$nEff, method=0)
         
-        # Generate output table with model comparison information for each variable
+        # Generate output table with t1.dem.env model information and model comparison info for each variable
         data.frame(
           chr = unique(inner.tmp$chr),
           pos = unique(inner.tmp$pos),
@@ -213,7 +211,7 @@ model.output =
           AIC=c(AIC(t1.dem.env)),
           b_enviro=last(t1.dem.env$coef),
           se_enviro=last(t1.dem.env$se),
-          p_lrt=anovaFun(t1.demo, t1.dem.env))
+          p_lrt=anovaFun(t1.dem, t1.dem.env))
           
       } # End run for all j enviro var
 
@@ -226,33 +224,34 @@ model.output =
         # Extract data for 'j' environmental variable
         gathered_data %>% filter(enviro_var == j) -> inner.tmp
                 
-                # Do 100 permutations; 
+          # Do 100 permutations; 
           foreach(l=1:100, .combine = "rbind")%do%{
             message(paste("Permutation number:", l))
                   
             # Shuffle enviro data for 'j' enviro variable
             inner.tmp %>% mutate(suffle_value = sample(value)) -> inner.tmp.shuffle
                   
-            # Model allele freq for 'j' enviro variable
+            # Model allele freq
+            # Generate 3 models - a null model (t0), a model with just demography (t1.dem) and a model with demography and "j" enviro var (t1.dem.env)
             y <- inner.tmp.shuffle$af_nEff
             X.null <- model.matrix(~1, inner.tmp.shuffle)
             X.dem <- model.matrix(~as.factor(demography), inner.tmp.shuffle)
             X.dem.env <- model.matrix(~as.factor(demography)+value, inner.tmp.shuffle)
             t0 <- fastglm(x=X.null, y=y, family=binomial(), weights=inner.tmp.shuffle$nEff, method=0)
-            t1.demo <- fastglm(x=X.dem, y=y, family=binomial(), weights=inner.tmp.shuffle$nEff, method=0)
+            t1.dem <- fastglm(x=X.dem, y=y, family=binomial(), weights=inner.tmp.shuffle$nEff, method=0)
             t1.dem.env <- fastglm(x=X.dem.env, y=y, family=binomial(), weights=inner.tmp.shuffle$nEff, method=0)
                   
-                  # Generate output table with model comparison information for each variable for the shuffled data
+                # Generate output table with t1.dem.env model information and model comparison info for each variable
                 data.frame(
                   chr = unique(inner.tmp.shuffle$chr),
                   pos = unique(inner.tmp.shuffle$pos),
                   variable = j,
                   missing=seqMissing(genofile),
-                  data = "real",
+                  data = "permutation",
                   AIC=c(AIC(t1.dem.env)),
                   b_enviro=last(t1.dem.env$coef),
                   se_enviro=last(t1.dem.env$se),
-                  p_lrt=anovaFun(t1.demo, t1.dem.env))
+                  p_lrt=anovaFun(t1.dem, t1.dem.env))
                   
                 } # End for i permutation
               } # End run for j enviro var
@@ -266,14 +265,11 @@ model.output =
 
 # Generate folders and save output
 
-# Make folder for model outputs
-folder_name <- paste("data/processed/GEA/glms/glms_window_analysis")
-system(paste("mkdir", folder_name, sep = " "))
-
-# Make folder for chunk y
-folder_name <- paste("data/processed/GEA/glms/glms_window_analysis/LM_100perm_Bio-Oracle_window_", y, sep = "")
-system(paste("mkdir", folder_name, sep = " "))
+# Folder name for chunk c
+folder_name <- paste("data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_", c, sep = "")
 
 # Save file for window k in chunk y
-file_name <- paste("LM_100perm_Bio-Oracle_y", y, "chr", wins$chr[k], "start", wins$start[k], "stop", wins$end[k], sep = "_")
-save(model.output.test, file = paste(folder_name, "/" , file_name, ".Rdata", sep = "") )
+file_name <- paste("GLM_100perm_Bio-Oracle_chunk", c, "chr", wins$chr[k], "start", wins$start[k], "stop", wins$end[k], sep = "_")
+save(glm.model.output, file = paste(folder_name, "/" , file_name, ".Rdata", sep = "") )
+
+message("done")
