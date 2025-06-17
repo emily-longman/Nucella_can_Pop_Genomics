4fa819fcd7dcbefa439b5c6aa7e293967727bd34
Emily Longman <Emily.Longman@uvm.edu>
2025-06-17 08:38:28 -0400

    Make ncbi script

--- src/00_Genome_long_read/00_cat_reads.sh
+++ src/00_Genome_long_read/00_cat_reads.sh
@@ -34,15 +34,15 @@
 cd /netfiles/pespenilab_share/Nucella/raw/ONT/
 
 # Concatenate reads from each ONT cell
-cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0150_Sanford_NC3-PCR_FC2_11152023/20231115_1701_2E_PAS52172_fe64f551/fastq_pass/*.fastq.gz > FC1.ONT.nuc.fastq.gz
+cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0150_Sanford_NC3-PCR_FC2_11152023/20231115_1701_2E_PAS52172_fe64f551/fastq_pass/*.fastq.gz > FC1_ONT_NC3.fastq.gz
 
-cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0150_Sanford_NC3-PCR_05312023/20230531_1740_3G_PAQ05812_93506d2a/fastq_pass/*.fastq.gz > FC2.ONT.nuc.fastq.gz
+cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0150_Sanford_NC3-PCR_05312023/20230531_1740_3G_PAQ05812_93506d2a/fastq_pass/*.fastq.gz > FC2_ONT_NC3.fastq.gz
 
-cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC1_02282024/20240228_1655_2E_PAW14761_69937e95/fastq_pass/*.fastq.gz > FC3.ONT.nuc.fastq.gz
+cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC1_02282024/20240228_1655_2E_PAW14761_69937e95/fastq_pass/*.fastq.gz > FC3_ONT_NC3.fastq.gz
 
-cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC2_02282024/20240228_1655_3G_PAU83574_150a145e/fastq_pass/*.fastq.gz > FC4.ONT.nuc.fastq.gz
+cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC2_02282024/20240228_1655_3G_PAU83574_150a145e/fastq_pass/*.fastq.gz > FC4_ONT_NC3.fastq.gz
 
-cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC3_03062024/20240306_1657_1G_PAU75920_614ca090/fastq_pass/*.fastq.gz > FC5.ONT.nuc.fastq.gz
+cat /netfiles/pespenilab_share/Nucella/raw/ONT/PROM0172_Sanford_NC3-PCR_FC3_03062024/20240306_1657_1G_PAU75920_614ca090/fastq_pass/*.fastq.gz > FC5_ONT_NC3.fastq.gz
 
 # Concatenate reads into one fastq.gz file
-cat *.ONT.nuc.fastq.gz > FC_all.ONT.nuc.fastq.gz
+cat *_ONT_NC3.fastq.gz > FC_all.sONT.nuc.fastq.gz
