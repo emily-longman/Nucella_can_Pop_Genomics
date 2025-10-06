4cb6cee27a78c07d63865182d7374329bc740b4b
Emily Longman <Emily.Longman@uvm.edu>
2025-10-06 08:32:01 -0400

    Update 03_variant_calling_relaxed_filt.sh

--- src/02_fastq_to_vcf/03_call_variants/03_variant_calling_relaxed_filt.sh
+++ src/02_fastq_to_vcf/03_call_variants/03_variant_calling_relaxed_filt.sh
@@ -8,22 +8,22 @@
 #SBATCH --job-name=variant_calling_freebayes_relaxed_filt
 
 # Specify partition
-#SBATCH --partition=general
+#SBATCH --partition=week #general
 
 # Request nodes
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=30:00:00 
+#SBATCH --time=3-00:00:00 #30:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=60G 
+#SBATCH --mem=200 #60G 
 
 # Request CPU
 #SBATCH --cpus-per-task=4
 
 # Submit job array
-#SBATCH --array=1-631%25
+#SBATCH --array=240,617 #1-631%25
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out # Standard output
